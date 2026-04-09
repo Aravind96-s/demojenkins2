33d979f2d24ae1070c3793da048a60f04da6e9e4
Aravind96-s <aravindrudra1996@gmail.com>
2026-04-09 12:20:51 +0530

    jenkins

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-app.use(express.static('../frontend'));
+// Serve frontend correctly
+app.use(express.static(path.join(__dirname, '../frontend')));
 
+// API
 app.get('/api', (req, res) => {
     res.json({ message: "Hello from Backend 🚀" });
+});
+
+// Home route fallback
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.listen(3000, '0.0.0.0', () => {
